27d3c594100bba9868d99e66366d89d0347be9e5
racky7 <raj.chauhan000@gmail.com>
2023-08-22 10:34:39 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -2,30 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
-import React, {useState} from 'react';
-import alert from './components/alert'; 
- 
+import React, { useState } from 'react';
+import Alert from './components/Alert';
 
- 
 function App() {
   const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
     })
     setTimeout(() => {
       setAlert(null);
-    },1500);
+    }, 1500);
 
   }
 
-
-
-  const toggleMode = ()=> {
-    if(mode=== 'light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
@@ -37,30 +33,30 @@ function App() {
       //   document.title = 'TextUtils is Amazing Mode';
       // },1500);
     }
-    else{
-      setMode= ('light');
+    else {
+      setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
       document.title = 'TextUtils- Light Mode';
-      
+
     }
 
   }
-   
+
   return (
     <>
-     
-<Navbar title = "TextUtils"  mode ={mode} toggleMode={toggleMode}/> 
-<alert alert={alert}/> 
-<div className="container my-3">
 
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
 
-<Textform showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-{/* <About/> */}
-</div>
+        <Textform showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+        {/* <About/> */}
+        
+      </div>
 
-      </>
-    
+    </>
+
   );
 }
 
